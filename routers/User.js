468bd79245343a25c595f7b57c3746767bf1393b
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-09-25 20:25:26 +0530

    Update User.js

--- routers/User.js
+++ routers/User.js
@@ -4,11 +4,13 @@ const {
   Signup,
   Login,
   AccountDelete,
+  GetUserId,
 } = require("../controllers/UserController");
 const { verifyToken } = require("../config/Jwt");
 
 router.post("/signup", Signup);
 router.post("/login", Login);
+router.get("/userdetails/:userId", verifyToken, GetUserId);
 router.delete("/delete/:userId", verifyToken, AccountDelete);
 
 module.exports = router;
