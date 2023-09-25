8235cba7d345c92ecf9164c027edc153acba648f
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-09-26 00:02:29 +0530

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -3,4 +3,6 @@ const router = Router();
 
 router.use("/user", require("./User"));
 router.use("/post", require("./Post"));
+router.use("/comment", require("./Comment"));
+router.use("/like", require("./Likes"));
 module.exports = router;
